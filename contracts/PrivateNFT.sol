178d58a67a1cdf8021d660082f8b0304f3d8e8e4
reshmi2458 <reshmijain930@gmail.com>
2024-08-17 08:33:30 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("reshm","rsh")
+        ERC721("rsm","rs")
         Ownable(initialOwner)
     {}
 
